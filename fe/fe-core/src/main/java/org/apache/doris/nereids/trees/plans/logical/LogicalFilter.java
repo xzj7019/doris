698cdee7f81391750978ed6a2856ffa353588252
xzj7019 <xiongzhongjian@selectdb.com>
2025-02-08 14:50:45 +0800

    [opt] hbo pass two join with rf and single side filter

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalFilter.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalFilter.java
@@ -94,6 +94,13 @@ public class LogicalFilter<CHILD_TYPE extends Plan> extends LogicalUnary<CHILD_T
     }
 
     @Override
+    public String toHboString() {
+        return Utils.toSqlString("Filter[" + getGroupIdWithPrefix() + "]",
+                "predicates", getPredicate()
+        );
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalFilter.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalFilter.java
@@ -83,7 +83,8 @@ public class PhysicalFilter<CHILD_TYPE extends Plan> extends PhysicalUnary<CHILD
 
     @Override
     public String toHboString() {
-        return Utils.toSqlString("PhysicalFilter" + getGroupIdWithPrefix(),
+        // id.asInt() is different from logical filter and physical filter
+        return Utils.toSqlString("Filter[" + getGroupIdWithPrefix() + "]",
                 "predicates", getPredicate()
         );
     }
